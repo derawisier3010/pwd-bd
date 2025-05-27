c66726cd504540ec61e151294706e9ce927fc078
derawisier3010 <derawisier3010@gmail.com>
2025-05-27 19:52:22 +0700

    27 Mei 2025

--- ALPRO/tes.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-    for ( int i = 15; i > 10; i = i - 1) {
-        cout << i << endl ;
-    }
-}
---
+++ PWD/W3School/javascript/script.js
@@ -0,0 +1,41 @@
+function demo() {
+    document.getElementById("demo").innerHTML = "Hello JavaScript !";
+  }
+
+function coba() {
+    document.getElementById("coba").style.fontSize='35px';
+  }
+
+function tes() {
+    document.getElementById("tes").style.display='none';
+  }
+
+function coba2() {
+    document.getElementById("coba2").style.display='block';
+  }
+
+function coba4(){
+window.alert('Hello World!');
+}
+
+//Statment
+let x = 5;
+let y = 6;
+let z = x + y;
+document.getElementById("result").innerHTML= "Hasil dari x + y adalah: " + z;
+
+function myFunction() {
+  document.getElementById("demo1").innerHTML = "Hello Dera!";
+  document.getElementById("demo2").innerHTML = "How are you?";
+}
+
+//operators
+let a = 10;
+document.getElementById("operators").innerHTML = a;
+
+//icrement
+let b = 5;
+b++;
+let c = b;
+document.getElementById("demo4").innerHTML = c;
+
